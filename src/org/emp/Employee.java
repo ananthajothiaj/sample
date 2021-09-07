9792a87ce4f5d4ac75b710a8ebe188643bda9128
ananthajothiaj <ananthajothiaj@gmail.com>
2021-09-07 22:38:17 +0530

    user functionality

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -1,5 +1,15 @@
 package org.emp;
 
 public class Employee {
+	
+	private void userName() {
+		System.out.println("name");
+
+	}
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.userName();
+	}
 
 }
